8aef280349b53c581211a6e4584dfa191bb3e04f
Antti Onttonen <antti@neubit.fi>
2022-09-06 14:21:32 +0300

    part_5 exercise_5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import { useState } from 'react';
+
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false);
+
+  const showBlogDetails = { display: detailsVisible ? '' : 'none' };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button onClick={() => setDetailsVisible(!detailsVisible)}>
+          {detailsVisible ? 'hide' : 'view'}
+        </button>
+      </div>
+      <div style={showBlogDetails}>
+        <div>{blog.url}</div>
+        <div>
+          {blog.likes}
+          <button>like</button>
+        </div>
+        <div>{blog.author}</div>
+      </div>
+    </div>
+  );
+};
+
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: 'solid',
+  borderWidth: 1,
+  marginBottom: 5,
+};
 
 export default Blog;
